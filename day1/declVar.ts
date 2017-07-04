22068703b4705353c0fe8138b5e6ee2197b74404
Mayuresh Rawool <Mayuresh Rawool>
2017-07-05 01:52:48 +0530

    day1

---
+++ day1/declVar.ts
@@ -0,0 +1,52 @@
+var stdRoll:number=786
+//declear variable with type Number
+//var <variableName>: <dataType> = <value>;
+function getData(){
+
+return stdRoll
+}
+
+var str:string="Khiladi"
+//declear variable with type String
+function strGet(){
+
+    return str
+}
+
+var other="The"
+
+
+// variable identified type from its value//Declaring boolean
+var isTrue:boolean=true
+
+//string
+var str1:string="hello"
+var str2:string='world'
+
+//Declaring array 
+//1 using []
+
+
+var rollNo:number[]=[1,2,3,4,5,6]
+
+//using  array<>
+var empNames:Array<string>=['a','s','d','f','g']
+
+console.log(str1+'mayuresh'+getData())
+
+//Declaring enum
+enum St {mayu,ram,sham}
+var Name:St=St.mayu
+alert(Name)
+
+//Declaring any
+
+var test1:any=10
+console.log(test1)
+
+test1="hi friend"
+console.log(test1)
+
+
+
+
---
+++ day1/es6-Let.ts
@@ -0,0 +1,20 @@
+// diffrence between var ans let keyword
+
+var a=10
+if(true){
+   var a=20
+}
+ console.log(a)
+//print 20
+
+
+//let-ES6
+
+let b=12
+if(true){
+   let b=20
+}
+ console.log(b)
+
+//print 12
+
